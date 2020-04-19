e64a06e61c28546eb464795e6df2e557759c5976
Matteo Corradin <matteo@app24h.it>
2020-04-19 21:57:01 +0200

    Super primitive iOS support

--- ios/Classes/SwiftGdprDialogPlugin.swift
+++ ios/Classes/SwiftGdprDialogPlugin.swift
@@ -1,7 +1,7 @@
 import Flutter
 import UIKit
 
-//import PersonalizedAdConsent гугловская библиотека
+import PersonalizedAdConsent
 public class SwiftGdprDialogPlugin: NSObject, FlutterPlugin {
 
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -26,56 +26,43 @@ public class SwiftGdprDialogPlugin: NSObject, FlutterPlugin {
 
     private func checkConsent(result: @escaping FlutterResult, publisherId: String, privacyUrl: String) {
 
-        print(publisherId);
-        print(privacyUrl);
+        SwiftGdprDialogPlugin.initGDPR(key: publisherId, url:privacyUrl)
         
-        result(true);
+        result(PACConsentInformation.sharedInstance.consentStatus == PACConsentStatus.personalized);
     }
 
-// //   Регистрация ключа для gdpr
-//    public static func initGDPR(key: String)
-//    {
-//           PACConsentInformation.sharedInstance.requestConsentInfoUpdate(
-//                    forPublisherIdentifiers: ["вот тут ключ в массиве]")
-//                {(_ error: Error?) -> Void in
-//                    if let error = error {
-//                        print("ERROR \(error)")
-//                    } else {
-//                        print("Success GDPG")
-//                    }
-//                }
-//    }
-//
-//
-// //      Вызов окна gdpr
-//    func showConsent(url : URL ,from : UIViewController,tab : UITabBarController)
-//    {
-//        guard let privacyUrl = URL(string: Supports.shared.privateURL()),
-//            let form = PACConsentForm(applicationPrivacyPolicyURL: url) else {
-//                print("incorrect privacy URL.")
-//                return
-//        }
-//        form.shouldOfferPersonalizedAds = true
-//        form.shouldOfferNonPersonalizedAds = true
-//        form.shouldOfferAdFree = true
-//
-//        form.load {(_ error: Error?) -> Void in
-//            print("Load complete.")
-//            if let error = error {
-//                // Handle error.
-//                print("Error loading form: \(error.localizedDescription)")
-//            } else {
-//                form.present(from: from) { (error, userPrefersAdFree) in
-//                    if let error = error {
-//                        print("Maybe its error \(error)")
-//                    } else if userPrefersAdFree {
-//                        tab.selectedIndex = 4
-//                    } else {
-//                        // Check the user's consent choice.
-//                        let status = PACConsentInformation.sharedInstance.consentStatus
-//                    }
-//                }
-//            }
-//        }
-//    }
+    public static func initGDPR(key: String, url:String)
+    {
+           PACConsentInformation.sharedInstance.requestConsentInfoUpdate(
+                    forPublisherIdentifiers: [key])
+                {(_ error: Error?) -> Void in
+                    if let error = error {
+                    } else {
+                        if PACConsentInformation.sharedInstance.consentStatus == PACConsentStatus.unknown {
+                            
+                            guard let privacyUrl = URL(string: url),
+                                let form = PACConsentForm(applicationPrivacyPolicyURL: privacyUrl) else {
+                                    return
+                            }
+                            form.shouldOfferPersonalizedAds = true
+                            form.shouldOfferNonPersonalizedAds = true
+                            form.shouldOfferAdFree = false
+                            
+                            form.load {(_ error: Error?) -> Void in
+                                if let error = error {
+                                    // Handle error.
+                                } else {
+                                    if let vc = UIApplication.shared.keyWindow?.rootViewController {
+                                        form.present(from: vc) { (error, userPrefersAdFree) in
+                                                                              
+                                                                           }
+                                    }
+                                   
+                                }
+                            }
+                        }
+                    }
+                }
+    }
+
 }
